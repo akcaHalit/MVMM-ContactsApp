063559e2ce2249976697e411afcb57d9c0f41f4b
Halit Akca <halitakca070@gmail.com>
2023-08-17 16:59:06 +0300

    - Kisiler entity is created.
    - ContactDetailFragment.kt has a argument as kisi.
    
    - ContactDetailFragment.kt's args is ready to use
    
    - search_image.xml is added.
    
    - Menu directory is added. toolbar_menu is added and also designed. search_image.xml is used in it.

--- app/src/main/java/com/halitakca/mvmmkisileruygulamasi/ui/fragment/ContactDetailFragment.kt
+++ app/src/main/java/com/halitakca/mvmmkisileruygulamasi/ui/fragment/ContactDetailFragment.kt
@@ -1,10 +1,12 @@
 package com.halitakca.mvmmkisileruygulamasi.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.halitakca.mvmmkisileruygulamasi.R
 import com.halitakca.mvmmkisileruygulamasi.databinding.FragmentContactDetailBinding
 import com.halitakca.mvmmkisileruygulamasi.databinding.FragmentHomePageBinding
@@ -21,8 +23,24 @@ class ContactDetailFragment : Fragment() {
 
         binding.toolbarcontactdetail.title = "Contact Detail"
 
+        val bundle : ContactDetailFragmentArgs by navArgs()
+        val kisi = bundle.kisi
+
+        binding.editTextPersonName.setText(kisi.kisi_ad)
+        binding.editTextPersonNumber.setText(kisi.kisi_tel)
+
+        binding.buttonUpdate.setOnClickListener{
+            val kisi_ad = kisi.kisi_ad
+            val kisi_tel = kisi.kisi_tel
+
+            update(kisi.kisi_id,kisi_ad,kisi_tel)
+        }
 
         return binding.root
+    }
+
+    fun update(kisi_id: Int,kisi_ad: String, kisi_tel: String) {
+        Log.e("Contact Updated","$kisi_id : $kisi_ad - $kisi_tel")
     }
 
 
